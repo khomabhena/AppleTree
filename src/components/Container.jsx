5695f9ad59ead17c4e43c0f6f9d665a7a3831c92
Kholwani Mabhena <colwanymab@gmail.com>
2025-06-24 04:39:13 +0200

    reduced font size in input fields

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 const Container = ({children, name}) => {
   return (
     <div className=' w-sm md:w-md bg-gray-200 min-h-[600px] rounded-2xl shadow-2xl'>
-        <div className=' flex flex-row justify-between mx-4'>
+        <div className=' flex flex-row justify-between mx-2 md:mx-4'>
             <div className=''></div>
             <div className=''>
                 <img src="/ax.svg" width="64px" height="auto" alt="" /> 
             </div>
         </div>
-        <div className=' flex flex-row justify-between mx-4'>
+        <div className=' flex flex-row justify-between mx-2 md:mx-4'>
             <p></p>
             <p>Timeout = <span className=' font-medium'>4:27</span></p>
         </div>
         <div className=' flex justify-center'>
             <img className=' mt-6' src="/appletree-logo.png" width="224px" alt="" />
         </div>
-        <div className=' mt-4 mx-12 pb-12'>
+        <div className=' mt-4 mx-8 md:mx-12 pb-12'>
             <p className=' bg-[#a0d900] mt-2 mb-4 py-3 font-bold text-xl shadow-xl rounded-2xl text-gray-900'>{name}</p>
             {children}
         </div>
