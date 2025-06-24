922eb4606f0159a0dd0e7c6c47c015d83383fab4
Kholwani Mabhena <colwanymab@gmail.com>
2025-06-24 04:41:45 +0200

    reduced font size in button selection

--- src/components/ButtonSelection.jsx
+++ src/components/ButtonSelection.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const ButtonSelection = ({location, image, name}) => {
   return (
-    <NavLink to={location} className=' w-full mt-3 flex items-center gap-3 bg-gray-100 rounded-2xl px-4 py-3 shadow-2xl border border-gray-300 text-xl text-gray-800'>
+    <NavLink to={location} className=' w-full mt-3 flex items-center gap-3 bg-gray-100 rounded-2xl px-4 py-3 shadow-2xl border border-gray-300 text-sm md:text-base text-gray-700'>
       <img src={image} width="50px" height="50px" alt="" />
       <p className=' border-1 border-gray-400 h-[40px]'></p>
       <p>{name}</p>
