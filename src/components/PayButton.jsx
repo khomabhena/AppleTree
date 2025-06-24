61bf2766837d72795858be6700cfb3c709440421
Kholwani Mabhena <colwanymab@gmail.com>
2025-06-24 04:31:17 +0200

    adjusted pay button size

--- src/components/PayButton.jsx
+++ src/components/PayButton.jsx
@@ -9,9 +9,9 @@ const PayButton = ({location, name}) => {
     <div className=' mt-3 flex justify-between items-center'>
       <div className=' text-start'>
         <p className=' text-2xl font-medium'>$45.00</p>
-        <p className=' text-sm text-gray-500'>View Details</p>
+        <p className=' text-xs text-gray-500'>View Details</p>
       </div>
-      <NavLink to={location} className=' cursor-pointer bg-[#a0d900] px-4 min-w-[250px] mb-4 py-3 font-bold text-2xl shadow-2xl border border-gray-300 rounded-2xl text-gray-900' type="button">{name}</NavLink>
+      <NavLink to={location} className=' cursor-pointer bg-[#a0d900] px-4 min-w-[200px] mb-4 py-3 font-bold text-xl shadow-2xl border border-gray-300 rounded-2xl text-gray-900' type="button">{name}</NavLink>
     </div> 
   )
 }
